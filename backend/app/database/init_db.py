abaae23c6cbe9a68428f39086bb670392deddb6f
Elijorson Ferreira Güichardo (Elio) <elijorson1@yahoo.es>
2026-06-30 22:06:00 +0200

    Update init_db.py

--- backend/app/database/init_db.py
+++ backend/app/database/init_db.py
@@ -1,8 +1,10 @@
 from app.database.session import Base, engine
 
-# IMPORTANTE: asegurar que modelos se registran
+# IMPORTANTE: forzar carga de modelos
 from app.models.user import User  # noqa
 
 
 def create_database():
+    print(">>> CREANDO TABLAS EN BD")
     Base.metadata.create_all(bind=engine)
+    print(">>> TABLAS CREADAS")
