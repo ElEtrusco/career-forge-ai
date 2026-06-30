d436a8ede8c88d398e7e13a1b8cfa628d92f0e02
Elijorson Ferreira Güichardo (Elio) <elijorson1@yahoo.es>
2026-06-30 21:47:36 +0200

    Update router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
 
+from app.api.routes.cv import router as cv_router
+
 router = APIRouter()
+
+router.include_router(cv_router)
+
 
 @router.get("/ping")
 def ping():
