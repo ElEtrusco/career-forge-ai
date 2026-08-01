9da86c078685acef7451dc08d2c4656031d0cbe5
Elijorson Ferreira Güichardo (Elio) <elijorson1@yahoo.es>
2026-08-01 03:46:08 +0000

    Mensaje descriptivo

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -65,3 +65,15 @@ class UserService:
             return None
 
         return user
+    
+ @staticmethod
+    def get_by_id(
+        db: Session,
+        user_id: int
+    ) -> User | None:
+
+        return (
+            db.query(User)
+            .filter(User.id == user_id)
+            .first()
+        )   
