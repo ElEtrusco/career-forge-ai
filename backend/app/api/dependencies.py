c145a1485cb65e2711e849a3ba1ad7f52a2d48d6
Elijorson Ferreira Güichardo (Elio) <elijorson1@yahoo.es>
2026-08-01 03:42:03 +0000

    Mensaje descriptivo

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,19 +1,47 @@
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.session import get_db
-
 from app.core.security import decode_access_token
-
 from app.services.user_service import UserService
+
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/auth/login"
 )
 
+
 def get_current_user(
     token: str = Depends(oauth2_scheme),
     db: Session = Depends(get_db),
 ):
+
+    payload = decode_access_token(token)
+
+    if payload is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token",
+        )
+
+    user_id = payload.get("sub")
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token payload",
+        )
+
+    user = UserService.get_by_id(
+        db,
+        int(user_id),
+    )
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found",
+        )
+
+    return user
