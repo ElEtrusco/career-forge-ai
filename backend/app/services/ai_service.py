a66e285971c5c2d954d5422affe66354bf6e1813
Elijorson Ferreira Güichardo (Elio) <elijorson1@yahoo.es>
2026-08-02 18:03:42 +0000

    Mensaje descriptivo

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -1,7 +1,5 @@
-from app.core.config import settings
-
 from app.llm.base import LLMService
-from app.llm.ollama_provider import OllamaProvider
+from app.llm.factory import LLMFactory
 
 
 class AIService:
@@ -14,21 +12,7 @@ class AIService:
     """
 
     def __init__(self):
-        self.provider: LLMService = self._get_provider()
-
-    def _get_provider(self) -> LLMService:
-        """
-        Selecciona el proveedor configurado en .env.
-        """
-
-        provider = settings.LLM_PROVIDER.lower()
-
-        if provider == "ollama":
-            return OllamaProvider()
-
-        raise ValueError(
-            f"Unsupported LLM provider: {provider}"
-        )
+        self.provider: LLMService = LLMFactory.get_provider()
 
     async def chat(
         self,
@@ -45,53 +29,3 @@ class AIService:
             temperature=temperature,
             max_tokens=max_tokens,
         )
-
-
-
-"""from app.core.config import settings
-
-from app.llm.base import LLMService
-from app.llm.ollama_provider import OllamaProvider
-
-
-class AIService:
-    """
-    Servicio principal de IA.
-
-    Esta capa abstrae el proveedor LLM.
-    Los endpoints no deben saber si usamos Ollama,
-    OpenAI u otro proveedor.
-    """
-
-    def __init__(self):
-        self.provider: LLMService = self._get_provider()
-
-    def _get_provider(self) -> LLMService:
-        """
-        Selecciona el proveedor configurado en .env.
-        """
-
-        provider = settings.LLM_PROVIDER.lower()
-
-        if provider == "ollama":
-            return OllamaProvider()
-
-        raise ValueError(
-            f"Unsupported LLM provider: {provider}"
-        )
-
-    async def chat(
-        self,
-        messages: list[dict],
-        temperature: float = 0.3,
-        max_tokens: int = 2048,
-    ) -> str:
-        """
-        Ejecuta una conversación con el modelo.
-        """
-
-        return await self.provider.chat(
-            messages=messages,
-            temperature=temperature,
-            max_tokens=max_tokens,
-        )"""
